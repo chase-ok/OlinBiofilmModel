40b9ba462fd0c565c5a699310f1548c289cd63aa
Chase Kernan <chase.kernan@students.olin.edu>
2013-03-07 22:22:33 -0500

    Justification graphs

--- src/obm/justification.py
+++ src/obm/justification.py
@@ -1,125 +1,68 @@
 import numpy as np
-import orthopoly as op
 import cv2
 import models
-from pseudospectral import diffmat
-from scipy.linalg import solve
-from scipy.ndimage.filters import laplace
 from scipy import optimize
-from collections import deque
 from matplotlib import pyplot as plt
 from itertools import product
 
-def justify_gaussian():
+# -----------------------------------------------------------------------------
+# DIFFUSION
+# -----------------------------------------------------------------------------
+
+def justify_diffusion():
+    boundary_layer = 5
+    diffusion_const = 1.0
     cells = generate_static_biofilm()
 
-    #k = 0.01
-    #penetration, _ = calculate_penetration_for_k(cells, 5, k)
-    #print penetration
+    gaussian = lambda x: calc_diffusion_by_gaussian(cells, boundary_layer, x)
+    herman = lambda x: calc_diffusion_by_hermanowicz(cells, boundary_layer, x)
+    def iteration(x, dt=0.1, iters=5000):
+        return calc_diffusion_by_iteration(cells, boundary_layer, x, diffusion_const, 
+                                           dt=dt, iters=iters)
 
-    penetration, _ = calculate_penetration_for_k_d(cells, 5, 0.1, 1.0, plot_error=True)
-    print penetration
+    depletion = 0.1
+    exact = iteration(depletion, dt=0.1, iters=5000)
+    ignore = make_boundary_layer(cells, boundary_layer) > 0
+    penetration, _ = fit_to_values(gaussian, exact, ignore=ignore)
+    herman_const, _ = fit_to_values(herman, exact, guess=0.01, ignore=ignore)
+    make_diffusion_plot(exact, 'By Iteration', 'iteration')
+    make_diffusion_plot(gaussian(penetration), 'By Gaussian', 'gaussian')
+    make_diffusion_plot(herman(herman_const), 'By Hermanowicz', 'herman')
 
-    #gaussian = calculate_diffusion_by_gaussian(cells, 5, penetration)
-    #herman = calculate_diffusion_by_hermanowicz(cells, 5, k)
-    #iteration = calculate_diffusion_by_iteration(cells, 5, 0.05, 1.0)
-    #plt.figure(); plt.pcolor(gaussian, cmap=plt.hot()); plt.colorbar(); plt.title('Gaussian')
-    #plt.figure(); plt.pcolor(herman, cmap=plt.hot()); plt.colorbar(); plt.title('Hermanowicz')
-    #plt.figure(); plt.pcolor(iteration, cmap=plt.hot()); plt.colorbar(); plt.title('Iteration')
+    num_iters = np.logspace(0, 3, 50).astype(int)
+    def convergence(dt):
+        return [np.sum((iteration(depletion, dt=dt, iters=i) - exact)**2)
+                for i in num_iters]
+    
+    plt.clf(); plt.hold(True)
+    for dt in [0.1, 0.2, 1.0, 2.0, 5.0]:
+        plt.plot(num_iters, convergence(dt), lw=3, label='dt = %s'%dt)
+    plt.semilogx()
+    plt.legend()
+    plt.xlabel('Num Iterations')
+    plt.ylabel('Error')
+    plt.savefig('fig/diffusion_convergence.png')
 
-    #plot_penentration_vs_k(cells, [3, 5, 7, 10])
+def make_diffusion_plot(values, subtitle, file_name=None):
+    plt.clf()
+    plt.pcolor(values, cmap=plt.hot())
+    plt.ylim([0, values.shape[0]-1])
+    plt.xlim([0, values.shape[1]-1])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Media Concentration: ' + subtitle)
+    plt.colorbar()
 
-def plot_penentration_vs_k(cells, boundary_layers):
-    results = []
-    kRecips = np.linspace(1, 300, 20)
-    for boundary in boundary_layers:
-        results.append(zip(*[calculate_penetration_for_k(cells, boundary, 1.0/kRecip)
-                             for kRecip in kRecips]))
+    file_name = file_name or subtitle
+    plt.savefig('fig/diffusion_%s.png' % file_name)
 
-    plt.figure(); plt.hold(True)
-
-    for result, boundary in zip(results, boundary_layers):
-        plt.plot(kRecips, result[0], lw=3, label=('Boundary = %d' % boundary))
-    plt.ylabel('Penetration Depth')
-    plt.legend(loc='lower right')
-
-    plt.twinx()
-    for result, boundary in zip(results, boundary_layers):
-        plt.plot(kRecips, np.sqrt(result[1])/cells.size, lw=1, label='__none__')
-    plt.ylabel('Average Error')
-    plt.xlabel('2D/k')
-
-    plt.title('Gaussian Diffusion')
-
-def calculate_penetration_for_k(cells, boundary_layer, k, plot_error=False):
-    herman = calculate_diffusion_by_hermanowicz(cells, boundary_layer, k)
-
-    no_cells = np.logical_not(cells)
-    def error(penetration):
-        penetration = max(0.0001, penetration)
-        diff = calculate_diffusion_by_gaussian(cells, boundary_layer, penetration) - herman
-        diff[no_cells] = 0.0
-        return diff
-    residual = lambda x: np.sum(error(x)**2)
-
-    penetration = optimize.fmin(residual, 1.0)[0]
-    if plot_error:
-        plt.figure(); plt.pcolor(abs(error(penetration))); plt.colorbar(); plt.title('Error')
-    return penetration, residual(penetration)
-
-def calculate_penetration_for_k_d(cells, boundary_layer, k, d, plot_error=False):
-    iteration = calculate_diffusion_by_iteration(cells, boundary_layer, k, d)
+def calc_diffusion_by_gaussian(cells, boundary_layer, media_penetration):
     boundary = make_boundary_layer(cells, boundary_layer)
+    media = cv2.GaussianBlur(boundary, (0, 0), media_penetration)
+    media[boundary > 0] = boundary[boundary > 0]
+    return media 
 
-    no_cells = np.logical_not(cells)
-    def error(penetration):
-        penetration = max(0.0001, penetration)
-        diff = calculate_diffusion_by_gaussian(cells, boundary_layer, penetration) - iteration
-        #diff = calculate_diffusion_by_hermanowicz(cells, boundary_layer, penetration) - iteration
-        diff[boundary > 0] = 0.0
-        return diff
-    residual = lambda x: np.sum(error(x)**2)
-
-    penetration = optimize.fmin(residual, 1.0)[0]
-    if plot_error:
-        plt.figure(); plt.pcolor(error(penetration)); plt.colorbar(); plt.title('Error')
-    return penetration, residual(penetration)
-
-def generate_static_biofilm(image_file="biofilm.png"):
-    return cv2.imread(image_file, 0)[:64, 64:128].astype(float)
-
-def generate_random_biofilm(n, p, max_density):
-    grid = np.zeros((n, n), float)
-
-    daughters = deque([(0, n/2)])
-    num_available = int(n*n*max_density)
-
-    while daughters and num_available > 0:
-        row, col = daughters.pop()
-
-        for dr in [-1, 0, 1]:
-            for dc in [-1, 0, 1]:
-                if dr == dc: 
-                    continue
-
-                new_row, new_col = row+dr, col+dc
-                if not (0 <= new_row < n) or not (0 <= new_col < n):
-                    continue
-                if grid[new_row, new_col] > 0:
-                    continue
-
-                if np.random.random() < p:
-                    grid[new_row, new_col] = 1.0
-                    daughters.appendleft((new_row, new_col))
-                    num_available -= 1
-
-    return grid
-
-def calculate_diffusion_by_gaussian(cells, boundary_layer, media_penetration):
-    media = make_boundary_layer(cells, boundary_layer)
-    return cv2.GaussianBlur(media, (0, 0), media_penetration)
-
-def calculate_diffusion_by_hermanowicz(cells, boundary_layer, k):
+def calc_diffusion_by_hermanowicz(cells, boundary_layer, k):
     boundary = make_boundary_layer(cells, boundary_layer)
 
     r_max, c_max = cells.shape
@@ -152,19 +95,19 @@ def calculate_diffusion_by_hermanowicz(cells, boundary_layer, k):
                 media[r, c] = media_at(r, c)
     return media
 
-def calculate_diffusion_by_iteration(cells, boundary_layer, k, d, dt=0.1):
+def calc_diffusion_by_iteration(cells, boundary_layer, k, d, dt=0.1, iters=5000):
     boundary = make_boundary_layer(cells, boundary_layer)
     in_cells = cells > 0
     in_boundary = boundary > 0
 
     media = boundary.copy()
     sigma = np.sqrt(2*d*dt)
-    def step():
+
+    for _ in range(iters):
         cv2.GaussianBlur(media, (0, 0), sigma, dst=media)
         media[in_cells] -= k*dt*media[in_cells]
         media[in_boundary] = boundary[in_boundary]
 
-    for i in range(5000): step()
     return media
 
 def make_boundary_layer(cells, thickness, concentration=1.0):
@@ -175,44 +118,11 @@ def make_boundary_layer(cells, thickness, concentration=1.0):
     cv2.floodFill(media, None, (media.shape[0]-1, media.shape[1]/2), 2)
     return (media == 2).astype(float)*concentration
 
-def compute_diffusion_error(cells, u, p_range):
-    """
-    u = concentration
-    ds = possible diffusion constants
-    rs = possible poisson terms for live cells
-    """
-    laplacian = laplace(u, mode='reflect')
-    poisson = (cells > 0)*1.0
-    error = lambda x: laplacian - x[0]*poisson
-    residual = lambda x: np.sum(error(x)**2)
+# -----------------------------------------------------------------------------
+# SURFACE TENSION
+# -----------------------------------------------------------------------------
 
-    x, min_resid, info = optimize.fmin_l_bfgs_b(residual, 
-                                                [p_range[1]], 
-                                                bounds=[p_range],
-                                                approx_grad=True)
-
-    plt.pcolor(error(x))
-    plt.colorbar()
-    plt.show()
-    return min_resid, error(x), x[0]
-
-def justify_surface_tension(block_size=7):
-    cells = generate_static_biofilm()
-    cell_block = np.empty((block_size, block_size), np.uint8)
-    models._write_block(cell_block, cells, 23, 23, block_size)
-
-    tension = 1.0
-    power = calculate_power_for_tension(cell_block, tension)
-    print power
-    approx = calculate_probabilities_by_convolution(cell_block.copy(), power)
-    exact = calculate_probabilities_by_energy(cell_block.copy(), tension)
-    plt.figure(); plt.pcolor(cell_block)
-    plt.figure(); plt.pcolor(cells)
-    plt.figure(); plt.pcolor(approx, cmap=plt.hot()); plt.colorbar(); plt.title('Approx')
-    plt.figure(); plt.pcolor(exact, cmap=plt.hot()); plt.colorbar(); plt.title('Exact')
-    plt.show()
-
-def calculate_probabilities_by_convolution(cell_block, power):
+def calc_surface_tension_by_convolution(cell_block, power):
     tension_kernel = np.array([[1, 2, 1],
                                [2, 0, 2],
                                [1, 2, 1]], float)
@@ -226,23 +136,19 @@ def calculate_probabilities_by_convolution(cell_block, power):
     probability[cell_block > 0] = 0
     probability **= power
     return normalize_probability(probability)
-    
 
-def calculate_probabilities_by_energy(cell_block, tension):
+def calc_surface_tension_by_energy(cell_block, tension):
     cell_block = cell_block.astype(np.uint8)
 
-    def calculate_perimeter():
-        #contours, _ = cv2.findContours(cell_block.copy(), 
-        #                               cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        #return sum(cv2.arcLength(contour, True) for contour in contours)
+    def calc_perimeter():
         horizontal = np.sum(cell_block[:-1, :] != cell_block[1:, :])
         vertical = np.sum(cell_block[:, :-1] != cell_block[:, 1:])
         return horizontal + vertical
     
-    base_perimeter = calculate_perimeter()
-    def calculate_perimeter_change(index):
+    base_perimeter = calc_perimeter()
+    def calc_perimeter_change(index):
         cell_block[index] = 1
-        change = calculate_perimeter() - base_perimeter
+        change = calc_perimeter() - base_perimeter
         cell_block[index] = 0
         return change
 
@@ -251,10 +157,59 @@ def calculate_probabilities_by_energy(cell_block, tension):
         if cell_block[index] > 0:
             probability[index] = 0.0
         else:
-            d_perimeter = calculate_perimeter_change(index)
+            d_perimeter = calc_perimeter_change(index)
             probability[index] = np.exp(-tension*d_perimeter)
 
     return normalize_probability(probability)
+
+def justify_surface_tension():
+    block_size = 15
+
+    cells = generate_static_biofilm()
+    cell_block = np.empty((block_size, block_size), np.uint8)
+    models._write_block(cell_block, cells, 23, 23, block_size)
+    make_surface_tension_plot(cell_block, "Cells", 'cells')
+    plt.clf(); plt.pcolor(cell_block, cmap=plt.hot()); plt.savefig("fig/surface_tension_cell_block.png")
+
+    convolution = lambda x: calc_surface_tension_by_convolution(cell_block, x)
+    energy = lambda x: calc_surface_tension_by_energy(cell_block, x)
+
+    tension = 1.0
+    power, _ = fit_to_values(convolution, energy(tension))
+    make_surface_tension_plot(energy(tension), 'By Energy', 'energy')
+    make_surface_tension_plot(convolution(power), 'By Convolution', 'convolution')
+
+    tensions = np.linspace(0.1, 10.0)
+    powers, residuals = fit_to_func(convolution, energy, tensions)
+    plt.clf()
+    plt.plot(tensions, powers, 'k-', lw=3)
+    plt.xlabel('Tension')
+    plt.ylabel('Power')
+    plt.savefig('fig/surface_tension_tension_vs_power.png')
+
+    plt.clf()
+    plt.plot(tensions, residuals, 'r-', lw=3)
+    plt.ylabel('Error')
+    plt.xlabel('Tension')
+    plt.savefig('fig/surface_tension_tension_vs_power_error.png')
+    
+
+def make_surface_tension_plot(values, subtitle, file_name=None):
+    plt.clf()
+    plt.pcolor(values, cmap=plt.hot())
+    plt.ylim([0, values.shape[0]-1])
+    plt.xlim([0, values.shape[1]-1])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title("Surface Tension Probability: " + subtitle)
+    plt.colorbar()
+
+    file_name = file_name or subtitle
+    plt.savefig('fig/surface_tension_%s.png' % file_name)
+
+# -----------------------------------------------------------------------------
+# UTILS
+# -----------------------------------------------------------------------------
 
 def normalize_probability(probability):
     norm = probability.sum()
@@ -263,15 +218,20 @@ def normalize_probability(probability):
     else:
         return probability/norm
 
-def calculate_power_for_tension(cell_block, tension):
-    exact = calculate_probabilities_by_energy(cell_block.copy(), tension)
-
-    error = lambda x: calculate_probabilities_by_convolution(cell_block.copy(), x) - exact
+def fit_to_values(func, values, guess=1.0, ignore=None):
+    def error(x):
+        diffs = func(x) - values
+        if ignore is not None:
+            diffs[ignore] = 0
+        return diffs
     residual = lambda x: np.sum(error(x)**2)
 
-    power = optimize.fmin(residual, 1.0)[0]
-    plt.figure(); plt.pcolor(abs(error(power))); plt.colorbar(); plt.title('Error')
-    return power
+    optimal = optimize.fmin(residual, guess)[0]
+    return optimal, np.sqrt(residual(optimal))/float(values.size)
 
-if __name__ == '__main__':
-    justify_gaussian()
+def fit_to_func(approx_func, exact_func, func_values, ignore=None):
+    return zip(*[fit_to_values(approx_func, exact_func(v), ignore=ignore) 
+                 for v in func_values])
+
+def generate_static_biofilm(image_file="biofilm.png"):
+    return cv2.imread(image_file, 0)[:64, 64:128].astype(float)
